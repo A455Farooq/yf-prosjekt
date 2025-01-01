28a4886ebfb1f36a63ab5b4a5dbf6c9730156a62
Yasir <farooqmikhail455@gmail.com>
2025-01-01 23:07:23 +0100

    Add new features and update styles

--- yf.js
+++
@@ -1,6 +0,0 @@
-// yf.js - Example JavaScript Code
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript file linked successfully!');
-    alert('Welcome to the YF Project!');
-  });
-  
