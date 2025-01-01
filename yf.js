fc7e6813f20cf3b35bc5597322a8caf24774bf19
Yasir <farooqmikhail455@gmail.com>
2025-01-01 22:53:13 +0100

    Add yf.js and link it to index.html

---
+++ yf.js
@@ -0,0 +1,6 @@
+// yf.js - Example JavaScript Code
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript file linked successfully!');
+    alert('Welcome to the YF Project!');
+  });
+  
